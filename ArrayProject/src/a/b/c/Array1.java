88aa0a01d8a4ae548f72b0a97bd8d784d3442a4b
ShivNomercy <shivdaskadam670@gmail.com>
2022-02-05 13:01:13 +0530

    Array Data

---
+++ ArrayProject/src/a/b/c/Array1.java
@@ -0,0 +1,25 @@
+package a.b.c;
+import java.util.*;
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> ao = new ArrayList<Integer>();
+		         ao.add(10);
+		         ao.add(20);
+		         ao.add(10);
+		         ao.add(30);
+		         ao.add(10);
+		         ao.add(40);
+		         ao.add(50);
+		         ao.add(10);
+		         System.out.println("=========================");
+		        
+		         for(Integer i:ao)
+		         {
+		        	 System.out.println(i);
+		         }
+
+	}
+
+}
